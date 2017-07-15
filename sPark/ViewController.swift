f613a65737a2a30fcb43ac80c3397cfa7976935b
Kanishka Mitra <Kanishka@kanishkas-air.att.net>
2017-07-15 12:12:49 -0500

    Initial Commit

---
+++ sPark/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sPark
+//
+//  Created by Kanishka Mitra on 7/15/17.
+//  Copyright © 2017 Kanishka Mitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
